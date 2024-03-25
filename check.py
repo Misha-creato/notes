ffc4de0ad4db260550e4621a46382f3fea0e15cd
Iri O <oparina.iri@gmail.com>
2024-03-26 01:19:06 +0500

    Added check for json data

--- check.py
+++ check.py
@@ -12,3 +12,10 @@ def is_file_exists_or_empty(file: str):
     if os.path.isfile(file) and os.path.getsize(file) > 0:
         return True
     return False
+
+
+def is_load_json_valid(json_dict: dict):
+    required_fields = ['title', 'text', 'created_at', 'updated_at']
+    if all(field in json_dict for field in required_fields):
+        return True
+    return False
